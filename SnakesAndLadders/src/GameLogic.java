b62d00c102e3b96257d5f2dcbfdcf4e1c87335b5
StefanoAnzolut <stefanoanzolut@users.noreply.github.com>
2019-10-03 14:00:55 +0200

    Basic Changes to Game Logic
    Just some additional lines of code to the  game logic

--- SnakesAndLadders/src/GameLogic.java
+++ SnakesAndLadders/src/GameLogic.java
@@ -1,17 +1,23 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class GameLogic {
 
     public static void main(String[] args) {
         System.out.println("Let's start the game by first deciding on the board size!\n");
-        //separat board ? numme init board im main ?
+        System.out.println("What would you like the board size to be?");
         Scanner scanner_board = new Scanner(System.in);
-        System.out.println("How many squares should it have?");
-        int squares = scanner_board.nextInt();
-        int [] board = new int [squares];
+        int board_size = scanner_board.nextInt();
+        int [] board = new int[board_size]; // board size begrenzung ? min ? max ?
+        for( int i = 1; i <= board_size; i++)
+            board[i-1] = i; // square object noch anfügen
+                            // wo sind snakes und ladders?
 
         String[] player_names = new String[4];
         Scanner scanner_names = new Scanner(System.in);
+        System.out.println("How many players are playing?");
+        Scanner scanner_name_num = new Scanner(System.in);
+        //muss grösser als 2 sein
         System.out.println("What are the players name?");
 
         int player_counter = 0;
@@ -19,6 +25,9 @@ public class GameLogic {
             String read_names = scanner_names.nextLine();
             player_names[player_counter] = read_names;
             player_counter += 1;
+            //Erster Spieler aus der Liste ziehen
+            // Wieder hinten anfügen
+
 
         }
 
