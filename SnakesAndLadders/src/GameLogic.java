0d0402bc2165e057cfe0dc59e27dd92beeff8d32
S Anzolut <stefano.anzolut@gmail.com>
2019-10-03 18:38:44 +0200

    Working on the Game Logic

--- SnakesAndLadders/src/GameLogic.java
+++ SnakesAndLadders/src/GameLogic.java
@@ -7,33 +7,51 @@ public class GameLogic {
     // int z = die.rollDie();
 
     public static void main(String[] args) {
+
+        // BOARD
         System.out.println("Let's start the game by first deciding on the board size!\n");
         System.out.println("What would you like the board size to be?");
+
         Scanner scanner_board = new Scanner(System.in);
         int board_size = scanner_board.nextInt();
         int [] board = new int[board_size]; // board size begrenzung ? min ? max ?
-        for( int i = 1; i <= board_size; i++)
+        for( int i = 1; i <= board_size; i++){
             board[i-1] = i; // square object noch anfügen
                             // wo sind snakes und ladders?
-
-        String[] player_names = new String[4];
-        Scanner scanner_names = new Scanner(System.in);
+        }
+        // PLAYER INPUT
         System.out.println("How many players are playing?");
         Scanner scanner_name_num = new Scanner(System.in);
-        //muss grösser als 2 sein
-        System.out.println("What are the players name?");
+        int player_count = scanner_name_num.nextInt();
 
-        int player_counter = 0;
-        while(player_counter < 4 ) {
-            String read_names = scanner_names.nextLine();
-            player_names[player_counter] = read_names;
-            player_counter += 1;
-            //Erster Spieler aus der Liste ziehen
-            // Wieder hinten anfügen
-
-
+        if (player_count < 2 || player_count > 4){
+            System.out.println("Sorry, this game can only be played by 2-4 players");
+            System.exit(0);
         }
 
+
+        Player [] thePlayer = new Player[player_count];
+        System.out.println("What are the players name?");
+        //Scanner player_name = new Scanner(System.in);
+
+
+        //for (int i = 0; i <= player_count-1; i++){
+            //thePlayer[i] = player_names[i];
+        //}
+
+        String[] player_names = new String[player_count];
+        int temp_count = player_count;
+        while(temp_count > 0 ) {
+            Scanner player_name = new Scanner(System.in);
+            String read_names = player_name.nextLine();
+            player_names[temp_count-1] = read_names;
+            temp_count -= 1;
+            //thePlayer[player_count] =
+        }
+        // FIRST TURN
+
+        //Erster Spieler aus der Liste ziehen
+        // Wieder hinten anfügen
 
     }
 }
