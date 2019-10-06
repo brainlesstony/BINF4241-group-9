5ece070fa2b232a7257b5d1e59691f5a842c1d91
Fabio Sisi <fabio.sisi@bluewin.ch>
2019-10-06 12:18:08 +0200

    getters and setters implemented

--- SnakesAndLadders/src/Player.java
+++ SnakesAndLadders/src/Player.java
@@ -2,13 +2,14 @@ public class Player {
     private Player(String name_user){ // each Player will be initialized with a name (probably user input) and on which square the player is
         String name = name_user;
         int onSquare = 1;
+        
     }
 
-    public static get_name(){
-        return name;
+    public String get_name(){
+        return this.name;
     }
 
-    public static get_onsquare(){
+    public String get_onsquare(){
         return onsquare;
     }
 
