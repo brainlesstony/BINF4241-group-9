e06c392eea9ade937eb9ae4a95c8b677a930bfb8
Tony <tonyylyy@gmail.com>
2019-10-06 12:33:41 +0200

    Update Player.java

--- SnakesAndLadders/src/Player.java
+++ SnakesAndLadders/src/Player.java
@@ -1,19 +1,22 @@
 public class Player {
+    String name;
+    int onSquare;
+
     Player(String name_user){ // each Player will be initialized with a name (probably user input) and on which square the player is
         String name = name_user;
         int onSquare = 1;
     }
 
-    public static get_name(){
-        return name;
+    public String get_name(){
+        return this.name;
     }
 
-    public static get_onsquare(){
-        return onsquare;
+    public int get_onsquare(){
+        return this.onSquare;
     }
 
-    public static void move_forward(steps){
-        onboard += steps;
+    public void move_forward(int steps){
+        this.onSquare += steps;
     }
 
 
